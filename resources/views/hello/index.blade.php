b2a788049b3e31ecc9cb99f2ea73f4f43054df3c
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-05-01 12:10:57 +0900

    バリデーション、エラーをフォームごとに表示する P128

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,44 +8,57 @@
 @endsection
 
 @section('content')
-  <p>{{ $msg }}</p>
-@if (@count($errors) > 0 )
-  <div>
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-@endif
 
-<table>
-  <form action="/hello" method="post">
-    {{ csrf_field() }}
-    <tr>
-      <th>name: </th>
-      <td><input type="text" name="name" value="{{old('name')}}"></td>
-    </tr>
+  <p>{{$msg}}</p>
 
-    <tr>
-      <th>mail: </th>
-      <td><input type="text" name="mail" value="{{old('mail')}}"></td>
-    </tr>
+  @if (count($errors) > 0)
+  <p>入力に問題があります。再入力してください</p>
+  @endif
 
-    <tr>
-      <th>age: </th>
-      <td><input type="text" name="age" value="{{old('age')}}"></td>
-    </tr>
+  <table>
+    <form action="/hello" method="post">
+      {{ csrf_field() }}
 
-    <tr>
-      <th></th>
-      <td><input type="submit" value="send"></td>
-    </tr>
-  </form>
-</table>
+      @if ($errors->has('name'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{$errors->first('name')}}</td>
+        </tr>
+      @endif
+        <tr>
+          <th>name: </th>
+          <td><input type="text" name="name" value="{{old('name')}}"></td>
+        </tr>
 
+      @if ($errors->has('mail'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{$errors->first('mail')}}</td>
+        </tr>
+      @endif
+        <tr>
+          <th>mail: </th>
+          <td><input type="text" name="mail" value="{{old('mail')}}"></td>
+        </tr>
 
+      @if ($errors->has('age'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{$errors->first('age')}}</td>
+        </tr>
+      @endif
+        <tr>
+          <th>age: </th>
+          <td><input type="text" name="age" value="{{old('age')}}"></td>
+        </tr>
 
+        <tr>
+          <th></th>
+          <td><input type="submit" value="send"></td>
+        </tr>
+
+    </form>
+  </table>
 @endsection
 
 @section('footer')
