9148182ae48ba51e792233d6833335f19e557412
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-05-05 18:41:26 +0900

    バリデーション コントローラでエラーメッセージをカスタマイズ 条件に応じてルールを追加 p147

--- app/Http/Controllers/helloController.php
+++ app/Http/Controllers/helloController.php
@@ -32,17 +32,26 @@ class helloController extends Controller
     $rules = [
       'name' => 'required',
       'mail' => 'email',
-      'age' => 'numeric|between:0,150',
+      'age' => 'numeric'
     ];
 
     $messages = [
       'name.required' => '名前は必ず入力してください',
       'mail.email' => 'メールアドレスは必須です',
       'age.numeric' => '年齢を整数で記入ください',
-      'age.between' => '年齢は0~150の間で入力ください',
+      'age.min' => '年齢はゼロ歳以上で記入ください',
+      'age.max' => '年齢は200歳以下で記入ください',
     ];
 
     $validator = Validator::make($request->all(), $rules, $messages);
+
+    $validator->sometimes('age', 'min:0', function ($input) {
+      return !is_int($input->age);
+    });
+
+    $validator->sometimes('age', 'max:200', function($input){
+      return !is_int($input->age);
+    });
 
     if ($validator->fails()){
       return redirect('/hello')
