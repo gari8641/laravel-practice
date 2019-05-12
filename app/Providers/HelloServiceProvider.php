15e38e6327780b5bd3c7eaf9fda4e68ed9f24236
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-05-12 15:08:52 +0900

    バリデーション 拡張。入門書のとおりだとうまくいかない。useを変更した。 p153

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -5,17 +5,17 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Validation\Validator;
+//use Illuminate\Validation\Validator;
 use App\Http\Validators\HelloValidator;
+use Illuminate\Support\Facades\Validator;
 
 class HelloServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         //
-      $validator = $this->app['validator'];
-      $validator->resolver(function($translator, $data, $rules, $messages){
-        return new HelloValidator($translator, $data, $rules, $messages);
+      Validator::extend('hello', function($attribute, $value, $parameters, $validator){
+        return $value % 2 == 0;
       });
     }
 }
