0d4529283bc8bb8c1e28bc60361e2aded53dc43e
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-09-04 20:22:31 +0900

    DB insert P190

--- resources/views/hello/edit.blade.php
+++ resources/views/hello/edit.blade.php
@@ -10,12 +10,12 @@
 
 @section('content')
   <table>
-  <form action="/hello/edit method="post"">
+  <form action="/hello/edit" method="post"">
     {{ csrf_field() }}
     <input type="hidden" name="id" value="{{$form->id}}">
-    <tr> <th>name: </th><td><input type="text" name="name" value="{{$from->name}}"></td></tr>
-    <tr> <th>mail: </th><td><input type="text" name="mail" value="{{$from->mail}}"></td></tr>
-    <tr> <th>age: </th><td><input type="text" name="age" value="{{$from->age}}"></td></tr>
+    <tr> <th>name: </th><td><input type="text" name="name" value="{{$form->name}}"></td></tr>
+    <tr> <th>mail: </th><td><input type="text" name="mail" value="{{$form->mail}}"></td></tr>
+    <tr> <th>age: </th><td><input type="text" name="age" value="{{$form->age}}"></td></tr>
     <tr> <th></th><td><input type="submit" value="send"></td></tr>
   </form>
   </table>
