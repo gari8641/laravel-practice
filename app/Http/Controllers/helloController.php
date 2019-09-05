10795bba35ec084ff7acd813ac7008f6a61dcac2
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-09-05 22:14:09 +0900

    クエリビルダ select. where orWhereで演算記号による絞り込み検索 p202

--- app/Http/Controllers/helloController.php
+++ app/Http/Controllers/helloController.php
@@ -80,13 +80,12 @@ class HelloController extends Controller
 
   public function show(Request $request)
   {
-    $id = $request->id;
-    // URLにパラメタ指定すると指定したレコード表示できるやつ。
-    // メソッドチェーン
-    // where(フィールド名, 演算記号（以上とか以下とか未満とか）, 値)。first()は、ヒットしたレコードのうち、最初の1件だけを返す。get()はすべて返す。
-    //
-    $id = $request->id;
-    $items = DB::table('people')->where('id', '<=', $id)->get();
+    $name = $request->name;
+    // orWhereは、whereを指定した後に指定する
+    $items = DB::table('people')
+      ->where('name', 'like', '%' . $name . '%')
+      ->orWhere('mail', 'like', '%' . $name . '%')
+      ->get();
     return view('hello.show', ['items' => $items]);
   }
 
