5eeffc60e73177fc272b1cc6f8007c0b8b922638
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-09-09 21:19:20 +0900

    Eloquent app/Personクラスにプロパティを設定して拡張 p232

--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    // モデルクラスにプロパティを追加し独自に拡張する
+  public function getData()
+  {
+    return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+  }
 }
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,14 +9,10 @@
 
 @section('content')
  <table>
-   <tr> <th>Name</th><th>Mail</th><th>Age</th> </tr>
-    @foreach ($items as $item)
-      <tr>
-        <td>{{$item->name}}</td>
-        <td>{{$item->mail}}</td>
-        <td>{{$item->age}}</td>
-      </tr>
-    @endforeach
+   <tr> <th>Data</th> </tr>
+   @foreach ($items as $item)
+    <tr> <td>{{$item->getData()}}</td> </tr>
+   @endforeach
  </table>
 @endsection
 
