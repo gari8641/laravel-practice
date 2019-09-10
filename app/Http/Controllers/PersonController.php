c5b571950d09321a13e571bd5b838b5d6a54f21f
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-09-10 09:52:23 +0900

    Eloquent where p236

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -20,7 +20,10 @@ class PersonController extends Controller
 
   public function search(Request $request)
   {
+    // where (フィールド, 値)
     $item = Person::where('name', $request->input)->first();
+    // 全レコードを得たい場合は、where (フィールド, 値)->get
+    //
     $param = ['input' => $request->input, 'item' => $item];
     return view('person.find', $param);
   }
