14d94f386733c0d57bffd5d4e59f17cb6cf5077f
宮田英信 <miyataeishin@garis-MacBook-Air.local>
2019-09-10 20:26:34 +0900

    Eloquent グローバルスコープ 年齢20より大きいのに絞る場合の例 p243

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+// グローバルスコープのために追加
+use Illuminate\Database\Eloquent\Builder;
+
 class Person extends Model
 {
     // モデルクラスにプロパティを追加し独自に拡張する
@@ -28,6 +32,20 @@ class Person extends Model
   public function scopeAgeLessThan($query, $n)
   {
     return $query->where('age','<=', $n);
+  }
+
+  // グローバルスコープ p243
+  // boot()は、モデルの初期化専用のメソッド。静的メソッド
+  protected static function boot()
+  {
+    parent::boot();
+
+    // 第一引数はスコープの名前、
+    // 第二引数にクロージャ（Builderインスタンスが引数で渡される。これを使って絞り込み処理する）
+    // 年齢が20より大きいものだけに絞る
+    static::addGlobalScope('age', function (Builder $builder) {
+      $builder->where('age', '>', 20);
+    });
   }
 
 }
